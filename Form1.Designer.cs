f2d78ce959f8e70f62518e3997222a926a853e48
iambariz <61651210+iambariz@users.noreply.github.com>
2021-01-29 22:18:01 +0000

    Progress

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,48 +30,38 @@ namespace shutdown_timer
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // startButton
             // 
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.button1.Font = new System.Drawing.Font("Calibri", 10.25F, System.Drawing.FontStyle.Bold);
-            this.button1.Location = new System.Drawing.Point(388, 36);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Start";
-            this.button1.UseVisualStyleBackColor = true;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.startButton.Font = new System.Drawing.Font("Calibri", 10.25F, System.Drawing.FontStyle.Bold);
+            this.startButton.Location = new System.Drawing.Point(410, 36);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
-            // textBox1
+            // timer1
             // 
-            this.textBox1.Location = new System.Drawing.Point(339, 39);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(29, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(243, 39);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(29, 20);
-            this.textBox2.TabIndex = 2;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(146, 39);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(29, 20);
-            this.textBox3.TabIndex = 3;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // label1
             // 
@@ -82,13 +72,12 @@ namespace shutdown_timer
             this.label1.Size = new System.Drawing.Size(88, 17);
             this.label1.TabIndex = 4;
             this.label1.Text = "Shutdown in:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 9.25F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(97, 41);
+            this.label2.Location = new System.Drawing.Point(87, 41);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(43, 15);
             this.label2.TabIndex = 5;
@@ -98,39 +87,98 @@ namespace shutdown_timer
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Calibri", 9.25F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(181, 41);
+            this.label3.Location = new System.Drawing.Point(182, 41);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(56, 15);
             this.label3.TabIndex = 6;
             this.label3.Text = "Minutes:";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Calibri", 9.25F, System.Drawing.FontStyle.Bold);
-            this.label4.Location = new System.Drawing.Point(278, 41);
+            this.label4.Location = new System.Drawing.Point(288, 42);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(55, 15);
             this.label4.TabIndex = 7;
             this.label4.Text = "Seconds:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(136, 39);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(242, 39);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown2.TabIndex = 10;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(349, 39);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown3.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(220, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "label6";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(497, 99);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.startButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -138,15 +186,17 @@ namespace shutdown_timer
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,30 +6,114 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Management;
 using System.Windows.Forms;
 
 namespace shutdown_timer
 {
     public partial class Form1 : Form
     {
+        int i = 0;
+        double counter;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        public void shutdown()
+        {
+            /*
+             uint32 Win32Shutdown(
+             [in] sint32 Flags,
+             [in] sint32 Reserved = 
+            );
+
+            WMIServiceObject = GetObject(
+    "Winmgmts:{impersonationLevel=impersonate,(Debug,Shutdown)}")
+    ForEach ComputerObject In WMIServiceObject.InstancesOf("Win32_OperatingSystem") 
+    testResult = ComputerObject.Win32Shutdown(2 + 4, 0) 
+    'reboot
+    'testResult = ComputerObject.Win32Shutdown(0, 0) 'logoff 
+    ' testResult = ComputerObject.Win32Shutdown(8 + 4, 0) 'shutdown 
+
+            */
+
+            ManagementClass mClass = new ManagementClass("Win32_OperatingSystem");
+            mClass.Get();
+            ManagementBaseObject mboClass_shutdown = mClass.GetMethodParameters("Win32Shutdown");
+            label5.Text = mboClass_shutdown.GetType().ToString();
+            mClass.Scope.Options.EnablePrivileges = true;
+            mboClass_shutdown["Flags"] = 1;
+            mboClass_shutdown["Reserved"] = 0;
+            foreach (ManagementObject moClass in mClass.GetInstances())
+            {
+              //moClass.InvokeMethod("Win32Shutdown", mboClass_shutdown, null);
+            }
+        }
+
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
             this.Text = "Shutdown Timer by Bariz";
         }
 
-        private void label1_Click(object sender, EventArgs e)
+
+        private void startButton_Click(object sender, EventArgs e)
         {
+            //label5.Text = "Hello world";
+            timer1.Enabled = true;
+            startButton.Text = "Stop";
+            if (counter > 0)
+            {
+                counter = 0;
+                timer1.Enabled = false;
+                startButton.Text = "Start";
+                label5.Text = counter.ToString();
+            }
+            getNum();
 
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        public void timer1_Tick(object sender, EventArgs e)
         {
+            displayNums(counter);
+            label6.Text = Convert.ToString(counter);
+            //label5.Text = "i:" + i++;
+            if(counter == 0)
+            {
+                timer1.Enabled = false;
+                shutdown();
+            }
+            counter--;
+        }
 
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+        public void getNum()
+        {
+            counter = (Convert.ToInt32(numericUpDown1.Value) * 3600) +
+                             (Convert.ToInt32(numericUpDown2.Value) * 60) +
+                             Convert.ToInt32(numericUpDown3.Value);
+           //label6.Text = Convert.ToString(inMillisec);
+        }
+
+        public void displayNums(double totalMs)
+        {   
+
+            double hour = Math.Floor(totalMs / 3600);
+
+            double mins = Math.Floor((totalMs - (hour * 3600)) / 60);
+            
+            double seconds = Math.Floor((totalMs - (hour * 3600) - (mins * 60) ));
+
+            numericUpDown1.Value = Convert.ToDecimal(hour);
+            numericUpDown2.Value = Convert.ToDecimal(mins);
+            numericUpDown3.Value = Convert.ToDecimal(seconds);
         }
     }
 }
